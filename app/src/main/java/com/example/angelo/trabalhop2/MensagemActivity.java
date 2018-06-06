d6860fd37414b2ed3b303674906bcadd58092a8e
Angelo <angelorig@gmail.com>
2018-06-06 17:40:38 -0300

    Activity Mensagem

--- app/src/main/java/com/example/angelo/trabalhop2/MensagemActivity.java
+++ app/src/main/java/com/example/angelo/trabalhop2/MensagemActivity.java
@@ -1,13 +1,32 @@
 package com.example.angelo.trabalhop2;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class MensagemActivity extends AppCompatActivity {
+
+    private TreinoDbHelper base;
+    private SQLiteDatabase db;
+    private Spinner spinner;
+    private EditText mensagem;
+    private ListView listView;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagem);
+
+        base = new TreinoDbHelper(getApplicationContext());
+        mensagem = (EditText)findViewById(R.id.edMensagem);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spDestinatario);
+        ArrayAdapter<Usuario> arrayAd = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, base.consultarUsuario());
+        spinner.setAdapter(arrayAd);
     }
 }
